5a6ab0f6d1afc901e697b85a0ea8aecfb8167f1f
Handy Menny <handymenny@outlook.com>
2021-05-02 21:21:21 +0200

    Avoid throwing an error while responding to non-ticket messages

--- src/files.ts
+++ src/files.ts
@@ -53,6 +53,9 @@ function fileHandler(type, bot, ctx) {
       userid = replyText.match(new RegExp('#T' +
             '(.*)' + '\n' + config.language.from));
     }
+	if (userid === null || userid === undefined) {
+      return;
+	}
   }
   forwardFile(bot, ctx, function(userInfo) {
     let receiverId = config.staffchat_id;
